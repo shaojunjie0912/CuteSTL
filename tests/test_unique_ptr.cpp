d25d7a660514db4c34e487a38a5ee13d8010600e
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-10-22 23:57:37 +0800

    补充一点

--- tests/test_unique_ptr.cpp
+++ tests/test_unique_ptr.cpp
@@ -10,15 +10,25 @@
 
 namespace cutestl {}
 
-// 创建一个函数对象的删除器（避免使用 std::function）
+// 创建一个可调用对象的删除器（避免使用 std::function）
 template <typename T>
 struct DefaultDeleter {
     void operator()(T* p) const {
+        std::cout << "DefaultDeleter: delete" << std::endl;
         delete p;  // 默认使用 delete 释放内存
     }
 };
 
-// 对文件类型的偏特化
+// DefaultDeleter 对数组类型的偏特化
+template <typename T>
+struct DefaultDeleter<T[]> {
+    void operator()(T* p) const {
+        std::cout << "DefaultDeleter: delete []" << std::endl;
+        delete[] p;
+    }
+};
+
+// DefaultDeleter 对文件类型的全特化
 template <>
 struct DefaultDeleter<FILE> {
     void operator()(FILE* p) const { fclose(p); }
@@ -28,7 +38,8 @@ template <typename T, typename Deleter = DefaultDeleter<T>>
 class UniquePtr {
 public:
     UniquePtr() : ptr_(nullptr) {}
-    UniquePtr(T* ptr, Deleter deleter = DefaultDeleter<T>{}) : ptr_(ptr), deleter_(deleter) {}
+    // NOTE: 闪回：避免非指向动态内存的指针隐式构造
+    explicit UniquePtr(T* ptr) : ptr_(ptr) {}
     ~UniquePtr() {
         if (ptr_) {
             Deleter{}(ptr_);
@@ -54,17 +65,40 @@ public:
         return *this;
     }
 
+    // NOTE: -> 自己递归下去直到找到原始指针
+    // 编译器能解引用
+    T* operator->() { return ptr_; }
     T* Get() { return ptr_; }
 
 private:
     T* ptr_ = nullptr;
-    Deleter deleter_;
+};
+
+// UniquePtr 针对 T[] 的偏特化
+// NOTE: 偏特化模板参数不能有默认值
+// 由于Deleter = DefaultDeleter<T>
+// 则 UniquePtr<T[], Deleter> 中 T[] 传递给 DefaultDeleter
+// NOTE: 这里使用继承是偷懒么？这个偏特化继承主模板
+template <typename T, typename Deleter>
+class UniquePtr<T[], Deleter> : public UniquePtr<T, Deleter> {};
+
+// ---------------------------------
+
+// template <typename T, typename... Args>
+// UniquePtr<T> MakeUnique(Args... args) {
+//     return UniquePtr<T>{new T(args...)};
+// }
+
+struct MyClass {
+    int a, b, c;
 };
 
 int main() {
-    auto a{UniquePtr<FILE>{new FILE}};
-    // auto a{UniquePtr<FILE>{fopen("build/tests/a.txt", "r")}};
-    // auto b{UniquePtr<FILE>{fopen("build/tests/b.txt", "r")}};
-    // std::cout << (char)fgetc(a.Get()) << std::endl;
+    // MyClass my_class1(1, 2, 3);
+    MyClass my_class2{1, 2, 3};
+    auto arr = UniquePtr<int>{new int(2)};
+    // auto my_class_ptr = MakeUnique<MyClass>(1, 2, 3);
+    // std::cout << my_class_ptr->a << std::endl;
+
     return 0;
 }
