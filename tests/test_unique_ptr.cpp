d846f90104d9ec2aa84e8f430c65e5744c4b842e
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-10-28 23:34:26 +0800

    更新了 UniquePtr

--- tests/test_unique_ptr.cpp
+++ tests/test_unique_ptr.cpp
@@ -1,7 +1,10 @@
+#if __cplusplus >= 202002L
 #include <concepts>
+#endif
 #include <cstdio>
 #include <iostream>
 #include <memory>
+#include <type_traits>
 #include <utility>
 #include <vector>
 
@@ -18,61 +21,61 @@ struct DefaultDeleter {
 };
 
 // DefaultDeleter 对数组类型的偏特化
-// template <typename T>
-// struct DefaultDeleter<T[]> {
-//     void operator()(T* p) const {
-//         std::cout << "DefaultDeleter: delete []" << std::endl;
-//         delete[] p;
-//     }
-// };
-
-// DefaultDeleter 对文件类型的全特化
-template <>
-struct DefaultDeleter<FILE> {
-    void operator()(FILE* p) const { fclose(p); }
+// NOTE: 这里 T[] 其实有所特指
+// 数组一般会退化为指针
+template <typename T>
+struct DefaultDeleter<T[]> {
+    void operator()(T* p) const {
+        // std::cout << "DefaultDeleter: delete []" << std::endl;
+        delete[] p;
+    }
 };
 
 template <typename T, typename Deleter = DefaultDeleter<T>>
 class UniquePtr {
+private:
+    T* ptr_ = nullptr;  // 原始指针
+    Deleter deleter_;   // 删除器
+
     // NOTE: 友元类，为了能在 std::exchange 中访问 other.ptr_
     template <typename U, typename UDeleter>
     friend class UniquePtr;
 
 public:
-    // NOTE: 为了能支持 UniquePtr = nullptr;
+    // NOTE: 为了能支持
+    // 1. UniquePtr = nullptr;
+    // 2. 空构造()
     // 也可以直接使用 UniquePtr.Reset() 方法
-    // 这个也支持默认构造 UniquePtr()
-    UniquePtr(std::nullptr_t dummy = nullptr) : ptr_(nullptr) {}
+    UniquePtr(std::nullptr_t dummy = nullptr) noexcept : ptr_(nullptr) {}
 
-    // NOTE: 闪回：避免非指向动态内存的指针隐式构造
-    explicit UniquePtr(T* ptr) : ptr_(ptr) {}
+    // NOTE: 闪回：避免由普通指针而不是指向动态内存的指针隐式构造
+    explicit UniquePtr(T* ptr) noexcept : ptr_(ptr) {}
 
-    ~UniquePtr() {
+    ~UniquePtr() noexcept {
         if (ptr_) {
-            Deleter{}(ptr_);  // NOTE: Delter{} 创建一个临时对象
+            deleter_(ptr_);
         }
     }
 
 public:
+    // 删除 cpctor
     UniquePtr(UniquePtr const& other) = delete;
 
-    UniquePtr(UniquePtr&& other) {
-        Deleter{}(ptr_);
-        ptr_ = other.ptr_;
-        other.ptr_ = nullptr;
-    }
+    // mvctor
+    UniquePtr(UniquePtr&& other) : ptr_(other.ptr_) { other.ptr_ = nullptr; }
 
 public:
-    // 拷贝赋值运算符
+    // 删除 cpassign
     UniquePtr& operator=(UniquePtr const& other) = delete;
 
-    // 移动赋值运算符
+    // mvassign
     UniquePtr& operator=(UniquePtr&& other) {
         // 判断是否自赋值
         if (this != &other) [[likely]] {
-            fclose(ptr_);
-            ptr_ = other.ptr_;
-            other.ptr_ = nullptr;
+            if (ptr_) {
+                deleter_(ptr_);
+            }
+            std::exchange(other.ptr_, nullptr);
         }
         return *this;
     }
@@ -89,16 +92,46 @@ public:
     // NOTE: 返回引用
     T& operator*() const { return *ptr_; }
 
+    // TODO: 不知道这是啥，而且还报错
+    // std::add_lvalue_reference_t<T> operator*() const noexcept { return *ptr_; }
+
+    explicit operator bool() const noexcept { return ptr_ != nullptr; }
+
+// bool operator==(UniquePtr const& other)const noexcept{
+//     return ptr_==
+// }
+#if __cplusplus >= 202002L
+    bool operator<=>(UniquePtr const& other) const noexcept { return ptr_ <=> other.ptr_; }
+#else
+    bool operator==(UniquePtr const& other) const noexcept { return ptr_ == other.ptr_; }
+
+    bool operator!=(UniquePtr const& other) const noexcept { return ptr_ != other.ptr_; }
+
+    bool operator<(UniquePtr const& other) const noexcept { return ptr_ < other.ptr_; }
+
+    bool operator<=(UniquePtr const& other) const noexcept { return ptr_ <= other.ptr_; }
+
+    bool operator>(UniquePtr const& other) const noexcept { return ptr_ > other.ptr_; }
+
+    bool operator>=(UniquePtr const& other) const noexcept { return ptr_ >= other.ptr_; }
+
+#endif
+
     // 类型转换（支持多态）
     // NOTE: new 原始指针支持 U* -> T*
     // 这里希望支持 UniquePtr<U> -> UniquePtr<T>
     // 法一：类型转换运算符
     // operator U() const {}
-    // 法二：采用构造函数的隐式转换
+    // 法二：采用构造函数的隐式转换 [x]
+#if __cplusplus >= 202002L
     template <typename U, typename UDeleter>
-        requires(std::convertible_to<U*, T*>)  // 确保可以转换
-    UniquePtr(UniquePtr<U, UDeleter> other) {
-        ptr_ = std::exchange(other.ptr_, nullptr);  // 交换后置空
+        requires(std::convertible_to<U*, T*>)  // C++20 ->
+#else
+    template <typename U, typename UDeleter,
+              std::enable_if_t<std::is_convertible_v<U*, T*>, int> = 0>  // C++11 ->
+#endif
+    UniquePtr(UniquePtr<U, UDeleter>&& other) noexcept {
+        ptr_ = std::exchange(other.ptr_, nullptr);
     }
 
 public:
@@ -108,13 +141,13 @@ public:
 
     void Reset(T* other_ptr = nullptr) {
         if (ptr_) {
-            Deleter{}(ptr_);
+            deleter_(ptr_);
         }
         ptr_ = other_ptr;
     }
 
-private:
-    T* ptr_ = nullptr;
+    void Swap(UniquePtr& other) noexcept { std::swap(ptr_, other.ptr_); }
+    Deleter GetDeleter() const noexcept { return deleter_; }
 };
 
 // UniquePtr 针对 T[] 的偏特化
@@ -122,8 +155,14 @@ private:
 // 由于Deleter = DefaultDeleter<T>
 // 则 UniquePtr<T[], Deleter> 中 T[] 传递给 DefaultDeleter
 // NOTE: 这里使用继承是偷懒么？这个偏特化继承主模板
-// template <typename T, typename Deleer>
-// class UniquePtr<T[], Deleter> : public UniquePtr<T, Deleter> {};
+template <typename T, typename Deleter>
+class UniquePtr<T[], Deleter> : public UniquePtr<T, Deleter> {
+    using UniquePtr<T, Deleter>::UniquePtr;  // HACK: 继承构造函数
+
+    std::add_lvalue_reference_t<T> operator[](std::size_t i) {
+        return this->Get()[i];  // HACK: 此处用 this
+    }
+};
 
 // ---------------------------------
 
@@ -136,7 +175,7 @@ UniquePtr<T> MakeUnique(Args&&... args) {
     // NOTE: T{args...} 花括号初始化没有构造函数的类对象[C++11]
     return UniquePtr<T>{new T{std::forward<Args>(args)...}};
     // NOTE: T(args...) 圆括号初始化没有构造函数的类对象[only C++20]
-    // return UniquePtr<T>{new T(args...)};
+    // return UniquePtr<T>{new T(std::forward<Args>(args)...)};
 }
 
 struct Animal {
