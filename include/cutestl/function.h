93b3588ac11d70d9437cd1a034de4ab04a7bc193
shaojunjie0912 <shaojunjie0912@outlook.com>
2024-12-05 15:59:33 +0800

    初步完成 function

--- include/cutestl/function.h
+++ include/cutestl/function.h
@@ -6,7 +6,9 @@
 #include <typeinfo>
 #include <utility>
 
-// 参考实现: https://github.com/parallel101/stl1weekend/blob/main/_Function.hpp
+// 参考:
+// - https://github.com/parallel101/stl1weekend/blob/main/_Function.hpp
+// - https://github.com/parallel101/stl1weekend/blob/main/test_Function.cpp
 
 namespace cutestl {
 
@@ -24,7 +26,6 @@ private:
     public:
         virtual RT Call(Args... args) = 0;                    // 类型擦除后的统一接口
         virtual std::unique_ptr<FuncBase> Clone() const = 0;  // 原型模式，克隆当前函数对象
-        virtual std::type_info const& Type() const = 0;       // 获得函数对象类型信息
         virtual ~FuncBase() = default;
     };
 
@@ -34,21 +35,18 @@ private:
         Fn f_;  // 仿函数对象
 
     public:
-        // HACK: 其实就只传了一个仿函数对象..orz
-        // 为啥还要用可变参数
-        // 可能还会传 (std::in_place, args..) 就地构造
+        // HACK:
+        // - 直接传一个仿函数对象
+        // - 或者传 (std::in_place, args..) 就地构造 Fn f_ 对象
         template <typename... CArgs>
-        explicit FuncImpl(std::in_place_t, CArgs&&... args) : f_(std::forward<Args>(args)...) {}
+        explicit FuncImpl(std::in_place_t, CArgs&&... args) : f_(std::forward<CArgs>(args)...) {}
 
-        RT Call(Args&&... args) override { return std::invoke(f_, std::forward<Args>(args)...); }
+        RT Call(Args... args) override { return std::invoke(f_, std::forward<Args>(args)...); }
 
         std::unique_ptr<FuncBase> Clone() const override {
             // HACK: 这里也是就地构造 f_ 作为 FuncImpl 构造函数形参
             return std::make_unique<FuncImpl>(std::in_place, f_);
         }
-
-        // TODO: typeid
-        std::type_info const& Type() const override { return typeid(Fn); }
     };
 
     std::unique_ptr<FuncBase> base_;
@@ -56,10 +54,49 @@ private:
 public:
     Function() = default;
 
-    template <typename Fn, std::enable_if_t<std::is_invocable_r_v<RT, Fn, Args...> &&
-                                                std::is_copy_constructible_v<Fn>,
-                                            int> = 0>
-    Function(Fn&& f) : base_(std::make_unique<FuncImpl<Fn>>(std::forward<Fn>(f))) {}
+    // TODO: 这里 std::decay_t 一定要加么？
+    // HACK: 不加 explicit 为了 lambda 能隐式转换为 Function
+    template <typename Fn,
+              std::enable_if_t<std::is_invocable_r_v<RT, std::decay_t<Fn>, Args...> &&
+                                   std::is_copy_constructible_v<Fn> &&
+                                   !std::is_same_v<std::decay_t<Fn>, Function<RT(Args...)>>,
+                               int> = 0>
+    Function(Fn&& f)
+        : base_(std::make_unique<FuncImpl<std::decay_t<Fn>>>(std::in_place, std::forward<Fn>(f))) {}
+
+    Function(Function const& other) {  // 拷贝构造
+        if (!other.base_) {
+            base_ = nullptr;
+        } else {
+            base_ = other.base_->Clone();
+        }
+    }
+
+    Function& operator=(Function const& other) {  // 拷贝赋值
+        if (this == &other) {
+            return *this;
+        }
+        if (!other.base_) {
+            base_ = nullptr;
+        } else {
+            base_ = other.base_->Clone();
+        }
+        return *this;
+    }
+    Function(Function&&) = default;             // 默认移动构造
+    Function& operator=(Function&&) = default;  // 默认移动赋值
+
+    operator bool() const noexcept { return base_ != nullptr; }
+
+    // 这里不加&& 因为不是模板<->万能引用
+    // 为什么这里能使用 std::forward?
+    RT operator()(Args... args) const {
+        // HACK: 如果仿函数对象指针为空则报错
+        if (!base_) {
+            throw std::bad_function_call();
+        }
+        return base_->Call(std::forward<Args>(args)...);
+    }
 };
 
 }  // namespace cutestl
--- tests/test_function.cpp
+++ tests/test_function.cpp
@@ -1,7 +1,35 @@
 #include <cutestl/function.h>
+// #include <cutestl/function_xiaopeng.h>
 
 #include <iostream>
+#include <memory>
+#include <string>
 
 using namespace cutestl;
 
-int main() { Function<int> func; }
+using std::cout;
+using std::endl;
+
+int SimpleFunc(int a, int b) {
+    printf("[SimpleFunc] a = %d, b = %d\n", a, b);
+    return a + b;
+}
+
+struct Foo {
+    void operator()(int a, int b) { printf("[ClassOperator] a = %d, b = %d\n", a, b); }
+};
+
+int main() {
+    int a = 42, b = 1024;
+    Function<int(int, int)> f1{SimpleFunc};
+    f1(a, b);
+
+    auto lambda_func = []() { printf("[LambdaFunc] I am lambda!\n"); };
+    Function<void(void)> f2{lambda_func};
+    f2();
+
+    Foo foo{};
+    Function<void(int, int)> f3{foo};
+    f3(a, b);
+    return 0;
+}
